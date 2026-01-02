24c37f71fa44e737a112f4ce8a0d3329ff902e2f
Casey Peters <casey.peters@lattice.com>
2026-01-01 19:54:38 -0500

    add git dash extension

--- .config/shell/apps/git.sh
+++ .config/shell/apps/git.sh
@@ -1,2 +1,8 @@
 alias g='git '
 alias gco='git checkout origin/main'
+alias prs='git dash'
+
+# https://www.gh-dash.dev/getting-started/
+function installGithubDashExtension() {
+  gh extension install dlvhdr/gh-dash
+}
