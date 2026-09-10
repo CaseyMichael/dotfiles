79d188c028c125657d9ec1d90156e96909d2d5aa
Casey Peters <casey.peters@lattice.com>
2026-09-09 21:38:47 -0400

    kill all running overmind procs

--- config/shell/apps/overmind.sh
+++ config/shell/apps/overmind.sh
@@ -8,3 +8,11 @@ alias orw='overmind restart weaver'
 # export OVERMIND_SOCKET=$HOME/.overmind.sock
 # export OVERMIND_TMUX=1
 # export OVERMIND_CONFIG=$HOME/.overmind.tmux.conf
+
+# kill all overmind processes
+function overmind-kill-all() {
+  pgrep -f overmind | while read -r pid; do
+    pkill -P "$pid" 2>/dev/null
+    kill "$pid" 2>/dev/null
+  done
+}
